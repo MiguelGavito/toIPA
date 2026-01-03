da0f3c11e2e6debe18e4caaea03c9248440045c4
MiguelGavito <mgavitogzz@gmail.com>
2026-01-03 12:27:09 -0600

    added darwin configuration for mac use

--- flake.nix
+++ flake.nix
@@ -5,39 +5,42 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = [
-        (pkgs.python3.withPackages (ps: [
-          ps.flask 
-          ps.epitran
-          ps.setuptools
-          ps.flask-cors
-        ]))        
-        pkgs.nodejs
-        pkgs.yarn
-      ];
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
+        f nixpkgs.legacyPackages.${system}
+      );
+    in {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.python3.withPackages (ps: [
+              ps.flask
+              ps.epitran
+              ps.setuptools
+              ps.flask-cors
+            ]))
+            pkgs.nodejs
+            pkgs.yarn
+          ];
+        };
+        backend = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.python3.withPackages (ps: [
+              ps.flask
+              ps.epitran
+              ps.setuptools
+              ps.flask-cors
+            ]))
+          ];
+        };
+        frontend = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.yarn
+          ];
+        };
+      });
     };
-
-    devShells.x86_64-linux.backend = pkgs.mkShell {
-      buildInputs = [
-        (pkgs.python3.withPackages (ps: [
-          ps.flask 
-          ps.epitran
-          ps.setuptools
-          ps.flask-cors
-        ]))
-      ];
-    };
-
-    devShells.x86_64-linux.frontend = pkgs.mkShell {
-      buildInputs = [
-        pkgs.nodejs
-        pkgs.yarn
-      ];
-    };
-  };
 }
 
